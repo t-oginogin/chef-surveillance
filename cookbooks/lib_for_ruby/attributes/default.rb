e9845e7033b9ed4c7287b1dab33bd55e97761049
Tatsuya Ogi <t.oginogin@gmail.com>
2015-04-29 07:24:34 +0900

    mysql to sqlite3

--- cookbooks/lib_for_ruby/attributes/default.rb
+++ cookbooks/lib_for_ruby/attributes/default.rb
@@ -1,1 +1,1 @@
-default['lib_for_ruby']['packages'] = %w{make build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev libcurl4-openssl-dev libmysqlclient-dev libffi-dev}
+default['lib_for_ruby']['packages'] = %w{make build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt-dev libcurl4-openssl-dev libmysqlclient-dev libffi-dev libsqlite3-dev}
