733ad3f18e0103660c1e1250124e61812ae1c41a
Tatsuya Ogi <t.oginogin@gmail.com>
2015-05-01 17:56:17 +0900

    add sysv-rc-conf

---
+++ cookbooks/sysv-rc-conf/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: sysv-rc-conf
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'sysv-rc-conf' do
+  :install
+end
